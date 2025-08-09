a1a4e7bde6614759b8cde0093be5b6152e598260
Nathan Dias <diazstar65@gmail.com>
2025-08-09 13:00:07 -0300

    Update hamburger

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ window.addEventListener('scroll', () => {
     });
 });
 
+/* Menu hamburguer */
+
+const menuBtn = document.getElementById('menu-btn');
+const navMenu = document.getElementById('nav-menu');
+const menuLinks = navMenu.querySelectorAll('#nav-menu a'); // Pega todos os links do menu
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('is-active');
+    navMenu.classList.toggle('active');
+});
+
+// Fecha ao clicar em qualquer link
+ menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      menuBtn.classList.remove('is-active');
+      navMenu.classList.remove('active');
+    });
+  });
+
 /* Enviar mensagem por Whatsapp */
 function enviarWhatsapp() {
 const nome = document.getElementById("nome").value;
